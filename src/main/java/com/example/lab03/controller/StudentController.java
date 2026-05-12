8f48e6482228db49b9eadf86a935260a37eb21ef
luongchibao <bao105497@donga.edu.vn>
2026-05-12 13:58:13 +0700

    Change database to localhost SQL Server and add REST API

--- src/main/java/com/example/lab03/controller/StudentController.java
+++ src/main/java/com/example/lab03/controller/StudentController.java
@@ -1,19 +1,47 @@
+// package com.example.lab03.controller;
+
+// import com.example.lab03.service.StudentService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class StudentController {
+//     @Autowired
+//     private StudentService studentService;
+
+//     @GetMapping("/students")
+//     public String listStudents(Model model) {
+//         model.addAttribute("students", studentService.getAllStudents());
+//         return "students"; // students.html
+//     }
+// }
 package com.example.lab03.controller;
 
-import com.example.lab03.service.StudentService;
+import com.example.lab03.entity.Student;
+import com.example.lab03.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
-    @Autowired
-    private StudentService studentService;
 
-    @GetMapping("/students")
-    public String listStudents(Model model) {
-        model.addAttribute("students", studentService.getAllStudents());
-        return "students"; // students.html
+    @Autowired
+    private StudentRepository repository;
+
+    // GET ALL
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    // POST
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return repository.save(student);
     }
 }
